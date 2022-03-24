d5899e2da6f30b9e007d28cb0c5428c553fddb74
Dewen Zeng <dzeng2@nd.edu>
2022-03-23 22:40:44 -0400

    initial commit

--- train_model.py
+++ train_model.py
@@ -9,22 +9,32 @@
 # where 'data' is a folder containing the Challenge data and 'model' is a folder for saving your model.
 
 import sys
+import argparse
 from helper_code import is_integer
 from team_code import train_challenge_model
 
+# if __name__ == '__main__':
+#     # Parse the arguments.
+#     if not (len(sys.argv) == 3 or len(sys.argv) == 4):
+#         raise Exception('Include the data and model folders as arguments, e.g., python train_model.py data model.')
+
+#     # Define the data and model foldes.
+#     data_folder = sys.argv[1]
+#     model_folder = sys.argv[2]
+
+#     # Change the level of verbosity; helpful for debugging.
+#     if len(sys.argv)==4 and is_integer(sys.argv[3]):
+#         verbose = int(sys.argv[3])
+#     else:
+#         verbose = 1
+
+#     train_challenge_model(data_folder, model_folder, verbose) ### Teams: Implement this function!!!
+
 if __name__ == '__main__':
     # Parse the arguments.
-    if not (len(sys.argv) == 3 or len(sys.argv) == 4):
-        raise Exception('Include the data and model folders as arguments, e.g., python train_model.py data model.')
-
-    # Define the data and model foldes.
-    data_folder = sys.argv[1]
-    model_folder = sys.argv[2]
-
-    # Change the level of verbosity; helpful for debugging.
-    if len(sys.argv)==4 and is_integer(sys.argv[3]):
-        verbose = int(sys.argv[3])
-    else:
-        verbose = 1
-
-    train_challenge_model(data_folder, model_folder, verbose) ### Teams: Implement this function!!!
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_folder", type=str, default='e:/data/physionet22')
+    parser.add_argument("--model_folder", type=str, default='./model/')
+    parser.add_argument("--verbose", type=int, default=1)
+    args = parser.parse_args()
+    train_challenge_model(args.data_folder, args.model_folder, args.verbose) ### Teams: Implement this function!!!

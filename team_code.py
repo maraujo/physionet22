37f70609cadb8896b9e904041ea9e559a4c21783
Dewen Zeng <75713682+dewenzeng@users.noreply.github.com>
2022-04-06 17:12:08 -0400

    update a resnet model

--- team_code.py
+++ team_code.py
@@ -13,8 +13,10 @@ from helper_code import *
 import numpy as np, scipy as sp, scipy.stats, os, sys, joblib
 from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestClassifier
-import openl3
+import torch
+import torch.nn.functional as F
 import pandas as pd
+from resnet import ResNet1d
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -24,159 +26,75 @@ from sklearn.metrics import classification_report
 # Required functions. Edit these functions to add your code, but do not change the arguments.
 #
 ################################################################################
-
-POS = "position"
-TIME = "time"
-LABEL = "label"
-ID = "ID"
-TEMP_FILE = "/tmp/cache_challege_df.pkl"
-SECONDS_PER_EMBEDDING = 2
-classes = ['Present', 'Unknown', 'Absent']
-num_classes = len(classes)
-main_model = openl3.models.load_audio_embedding_model(input_repr="mel256", content_type="music",  embedding_size=512)
-TRAIN_SMALL_SAMPLE = False
-
-def get_embs_df_from_patient_data(num_patient_files, patient_files, data_folder, verbose):
-    patients_embs = []
-    for i in range(num_patient_files):
-        if verbose >= 2:
-            print('    {}/{}...'.format(i+1, num_patient_files))
-            
-        if TRAIN_SMALL_SAMPLE and i > 50:
-            break
-
-        # Load the current patient data and recordings.
-        current_patient_data = load_patient_data(patient_files[i])
-        num_locations = get_num_locations(current_patient_data)
-        recording_information = current_patient_data.split('\n')[1:num_locations+1]
-        recordings, frequencies = load_recordings(data_folder, current_patient_data, get_frequencies=True) 
-
-        #Extract Embeedings
-        embs_part, tss_part = openl3.get_audio_embedding(recordings, frequencies, hop_size=SECONDS_PER_EMBEDDING, batch_size=4,   verbose=1 , model=main_model)
-
-        #Prepare Features DataFrame
-        record_files = []
-        audios_embs = []
-        for i in range(num_locations):
-            entries = recording_information[i].split(' ')
-            filename = entries[2]
-            filepath = os.path.join(data_folder, filename)
-            recording_pos = recording_information[i].split(" ")[0]
-            audio_embs_df = pd.DataFrame(embs_part[i])
-            audio_embs_df[POS] = recording_pos
-            audio_embs_df[TIME] = tss_part[i]
-            audios_embs.append(audio_embs_df)
-        audios_embs_df = pd.concat(audios_embs)
-        audios_embs_df = audios_embs_df.reset_index(drop=True)
-
-        # If murmur present, remove places where it is not audible (not in #Murmur locations)
-        patient_data_series = pd.Series(current_patient_data.split('\n')[num_locations+1:])
-        murmur_locations = patient_data_series[patient_data_series.str.contains("Murmur location")].iloc[0].split(": ")[-1]
-        murmur_locations = murmur_locations.split("+")
-        if not "nan" in murmur_locations:
-            audios_embs_df = audios_embs_df[audios_embs_df[POS].isin(murmur_locations)]
-        
-        # Extract labels and use one-hot encoding.
-        current_labels = np.zeros(num_classes, dtype=int)
-        label = get_label(current_patient_data)
-        if label in classes:
-            j = classes.index(label)
-            current_labels[j] = 1
-        audios_embs_df[classes[0]] = current_labels[0]
-        audios_embs_df[classes[1]] = current_labels[1]
-        audios_embs_df[classes[2]] = current_labels[2]
-        audios_embs_df[ID] =  current_patient_data.split(" ")[0]
-        print(current_patient_data.split(" ")[0], audios_embs_df.shape)
-        patients_embs.append(audios_embs_df)
-    all_patients_embs_df = pd.concat(patients_embs)
-    return all_patients_embs_df
+SEQ_LENGTH = 24576
 
 # Train your model.
 def train_challenge_model(data_folder, model_folder, verbose):
-    # main_model = openl3.models.load_audio_embedding_model(input_repr="mel256", content_type="music",  embedding_size=512)
-    
-    # Find data files.
-    if verbose >= 1:
-        print('Finding data files...')
-
-    # Find the patient data files.
-    patient_files = find_patient_files(data_folder)
-    num_patient_files = len(patient_files)
-
-    if num_patient_files==0:
-        raise Exception('No data was provided.')
-
-    # Create a folder for the model if it does not already exist.
-    os.makedirs(model_folder, exist_ok=True)
-    # Extract the features and labels.
-    if verbose >= 1:
-        print('Extracting features and labels from the Challenge data...')
-    
-    if os.path.exists(TEMP_FILE):
-        all_patients_embs_df = pd.read_pickle(TEMP_FILE)
-    else:
-        all_patients_embs_df = get_embs_df_from_patient_data(num_patient_files, patient_files, data_folder, verbose)
-        try:
-            all_patients_embs_df.to_pickle(TEMP_FILE)
-        except:
-            print("Could not save TEMP file")
-
-    #Stratify Sample
-    all_patients_embs_df = all_patients_embs_df.reset_index(drop=True)
-    X_train, X_test, y_train, y_test = train_test_split(all_patients_embs_df[range(0,512)], all_patients_embs_df[classes], test_size=0.2, random_state=42, stratify= all_patients_embs_df[classes].values.argmax(axis=1))
-    
-
-    # Train the model.
-    if verbose >= 1:
-        print('Training model...')
-
-    # Define parameters for random forest classifier.
-    n_estimators = 100    # Number of trees in the forest.
-    max_leaf_nodes = 100 # Maximum number of leaf nodes in each tree.
-    random_state = 42   # Random state; set for reproducibility.
-
-    classifier = RandomForestClassifier(n_estimators=n_estimators, max_leaf_nodes=max_leaf_nodes, random_state=random_state, class_weight = [{0:5, 1: 100}, {0: 1, 1: 1}, {0: 1, 1: 5}])
-    classifier.fit(X_train, y_train)
-   
-    
-    #Check primarily results
-    if verbose >= 1:
-        print(classification_report(y_test, classifier.predict(X_test)))
-    
-    # Save the model.
-    save_challenge_model(model_folder, classes, classifier)
-
-    if verbose >= 1:
-        print('Done.')
+    # do nothing, we just want to test our pretrained model
+    return 0
 
 # Load your trained model. This function is *required*. You should edit this function to add your code, but do *not* change the
 # arguments of this function.
 def load_challenge_model(model_folder, verbose):
-    filename = os.path.join(model_folder, 'model.sav')
-    return joblib.load(filename)
+    # define model
+    model = ResNet1d(
+        input_dim=(1,SEQ_LENGTH), 
+        blocks_dim=list(zip([64, 128, 196, 256, 320], [4096, 1024, 256, 64, 16])), 
+        n_classes=3, 
+        kernel_size=17, 
+        dropout_rate=0.8,
+    )
+    # load the pretrained model
+    model_dict = torch.load(os.path.join(model_folder, 'model.pth'), map_location='cpu')
+    model.load_state_dict(model_dict)
+    model.eval()
+    return model
 
 # Run your trained model. This function is *required*. You should edit this function to add your code, but do *not* change the
 # arguments of this function.
 def run_challenge_model(model, data, recordings, verbose):
-    classes = model['classes']
-    classifier = model['classifier']
-    frequency_sample_rate = int(data.split("\n")[0].split(" ")[2])
-    
-    embs_part, tss_part = openl3.get_audio_embedding(recordings, [frequency_sample_rate]*len(recordings), hop_size=SECONDS_PER_EMBEDDING, batch_size=4,   verbose=1 , model=main_model)
-    
-    audio_results = []
-    for emb_parts in embs_part:
-        audio_segs = pd.DataFrame(emb_parts)
-        result = classifier.predict(pd.DataFrame(audio_segs)).max(axis=0)
-        audio_results.append(result)
-        
-    if pd.DataFrame(audio_results)[0].max() > 0.5:
-        labels = [1,0,0]
-    elif pd.DataFrame(audio_results)[1].max() > 0.5:
-        labels = [0,1,0]
-    else:
-        labels = [0,0,1]
-    probabilities = result
+    classes = ['Present', 'Unknown', 'Absent']
+    all_probabilities = []
+    all_labels = []
+    # do prediction for each recording
+    if verbose:
+        patient_id = get_patient_id(data)
+        print(f'processing patient_id:{patient_id}')
+    with torch.no_grad():
+        for recording in recordings:
+            # normalized recording
+            recording_mean = recording.mean()
+            recording_std = recording.std()
+            recording = (recording - recording_mean) / recording_std
+            # now, let's just random pick one segment from the recording to make prediction
+            current_recording_length = recording.shape[0]
+            if current_recording_length > SEQ_LENGTH:
+                start_point = 0
+                steps = []
+                while (start_point + SEQ_LENGTH) < current_recording_length:
+                    steps += [start_point]
+                    start_point += SEQ_LENGTH # non-overlap
+                steps += [current_recording_length - SEQ_LENGTH]
+                # print(f'steps:{steps}')
+                recording_segments = []
+                for step in steps:
+                    recording_segments.append(recording[step:step+SEQ_LENGTH])
+                recording_segment = torch.from_numpy(np.stack(recording_segments)[:,np.newaxis,:]).float()
+            else: # padding with zeros
+                recording_segment = np.zeros(SEQ_LENGTH)
+                recording_segment[0:current_recording_length] = recording
+                recording_segment = torch.from_numpy(recording_segment[None,None]).float()
+            # make prediction
+            out = model(recording_segment)
+            out_softmax = F.softmax(out, dim=1)
+            out_argmax = torch.argmax(out_softmax, dim=1)
+            # if there are multiple segments in this recording, the final prediction is the one with the largest "Present" probability
+            out_softmax_0 = out_softmax[:,0]
+            max_0_index = torch.argmax(out_softmax_0, dim=0)
+            all_probabilities.append(out_softmax[max_0_index].detach().cpu().numpy())
+            all_labels.append(out_argmax[max_0_index].detach().cpu().numpy())
+    labels = to_one_hot_bool(min(all_labels), len(classes))
+    probabilities = all_probabilities[all_labels.index(min(all_labels))]
     return classes, labels, probabilities
 
 ################################################################################
@@ -247,3 +165,12 @@ def get_features(data, recordings):
     features = np.hstack(([age], sex_features, [height], [weight], [is_pregnant], recording_features))
 
     return np.asarray(features, dtype=np.float32)
+
+def to_one_hot_bool(data, num_classes):
+    """
+    convert a numpy array to one hot bool
+    data: [N] the array that needs to be converted
+    num_classes: how many classes in this array
+    """
+    result = np.eye(num_classes)[data].astype(np.bool_)
+    return result

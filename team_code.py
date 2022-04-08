acc421d3cb6f5020d578a39268d9784652848c3d
matheus <0matheus.araujo0@gmail.com>
2022-04-08 01:00:21 +0000

    .

--- team_code.py
+++ team_code.py
@@ -11,10 +11,32 @@
 import shutil
 from pathlib import Path
 from helper_code import *
+<<<<<<< HEAD
 import numpy as np, pandas as pd, scipy as sp, scipy.stats, os, sys, joblib
 import openl3
 
+=======
+import numpy as np, scipy as sp, scipy.stats, os, sys, joblib
+from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestClassifier
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pandas as pd
+from resnet import ResNet1d
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import train_test_split
+>>>>>>> ad44dd9 (change to download from url)
 from sklearn.metrics import classification_report
+
+from urllib.error import HTTPError
+from urllib.request import urlopen, Request
+from urllib.parse import urlparse 
+
+import hashlib
+import shutil
+import tempfile
+from tqdm import tqdm
 
 ################################################################################
 #
@@ -112,8 +134,153 @@ def copy_test_data(test_ids, source, dest):
             shutil.copyfile(f, os.path.join(dest, os.path.basename(f)))
 
 
+# Model definition
+def _padding(downsample, kernel_size):
+    """Compute required padding"""
+    padding = max(0, int(np.floor((kernel_size - downsample + 1) / 2)))
+    return padding
+
+
+def _downsample(n_samples_in, n_samples_out):
+    """Compute downsample rate"""
+    downsample = int(n_samples_in // n_samples_out)
+    if downsample < 1:
+        raise ValueError("Number of samples should always decrease")
+    if n_samples_in % n_samples_out != 0:
+        raise ValueError("Number of samples for two consecutive blocks "
+                         "should always decrease by an integer factor.")
+    return downsample
+
+
+class ResBlock1d(nn.Module):
+    """Residual network unit for unidimensional signals."""
+
+    def __init__(self, n_filters_in, n_filters_out, downsample, kernel_size, dropout_rate):
+        if kernel_size % 2 == 0:
+            raise ValueError("The current implementation only support odd values for `kernel_size`.")
+        super(ResBlock1d, self).__init__()
+        # Forward path
+        padding = _padding(1, kernel_size)
+        self.conv1 = nn.Conv1d(n_filters_in, n_filters_out, kernel_size, padding=padding, bias=False)
+        self.bn1 = nn.BatchNorm1d(n_filters_out)
+        self.relu = nn.ReLU()
+        self.dropout1 = nn.Dropout(dropout_rate)
+        padding = _padding(downsample, kernel_size)
+        self.conv2 = nn.Conv1d(n_filters_out, n_filters_out, kernel_size,
+                               stride=downsample, padding=padding, bias=False)
+        self.bn2 = nn.BatchNorm1d(n_filters_out)
+        self.dropout2 = nn.Dropout(dropout_rate)
+
+        # Skip connection
+        skip_connection_layers = []
+        # Deal with downsampling
+        if downsample > 1:
+            maxpool = nn.MaxPool1d(downsample, stride=downsample)
+            skip_connection_layers += [maxpool]
+        # Deal with n_filters dimension increase
+        if n_filters_in != n_filters_out:
+            conv1x1 = nn.Conv1d(n_filters_in, n_filters_out, 1, bias=False)
+            skip_connection_layers += [conv1x1]
+        # Build skip conection layer
+        if skip_connection_layers:
+            self.skip_connection = nn.Sequential(*skip_connection_layers)
+        else:
+            self.skip_connection = None
+
+    def forward(self, x, y):
+        """Residual unit."""
+        if self.skip_connection is not None:
+            y = self.skip_connection(y)
+        else:
+            y = y
+        # 1st layer
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.dropout1(x)
+
+        # 2nd layer
+        x = self.conv2(x)
+        x += y  # Sum skip connection and main connection
+        y = x
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.dropout2(x)
+        return x, y
+
+class ResNet1d(nn.Module):
+    """Residual network for unidimensional signals.
+    Parameters
+    ----------
+    input_dim : tuple
+        Input dimensions. Tuple containing dimensions for the neural network
+        input tensor. Should be like: ``(n_filters, n_samples)``.
+    blocks_dim : list of tuples
+        Dimensions of residual blocks.  The i-th tuple should contain the dimensions
+        of the output (i-1)-th residual block and the input to the i-th residual
+        block. Each tuple shoud be like: ``(n_filters, n_samples)``. `n_samples`
+        for two consecutive samples should always decrease by an integer factor.
+    dropout_rate: float [0, 1), optional
+        Dropout rate used in all Dropout layers. Default is 0.8
+    kernel_size: int, optional
+        Kernel size for convolutional layers. The current implementation
+        only supports odd kernel sizes. Default is 17.
+    References
+    ----------
+    .. [1] K. He, X. Zhang, S. Ren, and J. Sun, "Identity Mappings in Deep Residual Networks,"
+           arXiv:1603.05027, Mar. 2016. https://arxiv.org/pdf/1603.05027.pdf.
+    .. [2] K. He, X. Zhang, S. Ren, and J. Sun, "Deep Residual Learning for Image Recognition," in 2016 IEEE Conference
+           on Computer Vision and Pattern Recognition (CVPR), 2016, pp. 770-778. https://arxiv.org/pdf/1512.03385.pdf
+    """
+
+    def __init__(self, input_dim, blocks_dim, n_classes, kernel_size=17, dropout_rate=0.8):
+        super(ResNet1d, self).__init__()
+        # First layers
+        n_filters_in, n_filters_out = input_dim[0], blocks_dim[0][0]
+        n_samples_in, n_samples_out = input_dim[1], blocks_dim[0][1]
+        downsample = _downsample(n_samples_in, n_samples_out)
+        padding = _padding(downsample, kernel_size)
+        self.conv1 = nn.Conv1d(n_filters_in, n_filters_out, kernel_size, bias=False,
+                               stride=downsample, padding=padding)
+        self.bn1 = nn.BatchNorm1d(n_filters_out)
+
+        # Residual block layers
+        self.res_blocks = []
+        for i, (n_filters, n_samples) in enumerate(blocks_dim):
+            n_filters_in, n_filters_out = n_filters_out, n_filters
+            n_samples_in, n_samples_out = n_samples_out, n_samples
+            downsample = _downsample(n_samples_in, n_samples_out)
+            resblk1d = ResBlock1d(n_filters_in, n_filters_out, downsample, kernel_size, dropout_rate)
+            self.add_module('resblock1d_{0}'.format(i), resblk1d)
+            self.res_blocks += [resblk1d]
+
+        # Linear layer
+        n_filters_last, n_samples_last = blocks_dim[-1]
+        last_layer_dim = n_filters_last * n_samples_last
+        self.lin = nn.Linear(last_layer_dim, n_classes)
+        self.n_blk = len(blocks_dim)
+
+    def forward(self, x):
+        """Implement ResNet1d forward propagation"""
+        # First layers
+        x = self.conv1(x)
+        x = self.bn1(x)
+
+        # Residual blocks
+        y = x
+        for blk in self.res_blocks:
+            x, y = blk(x, y)
+
+        # Flatten array
+        x = x.view(x.size(0), -1)
+
+        # Fully conected layer
+        x = self.lin(x)
+        return x
+
 # Train your model.
 def train_challenge_model(data_folder, model_folder, verbose):
+<<<<<<< HEAD
     # main_model = openl3.models.load_audio_embedding_model(input_repr="mel256", content_type="music",  embedding_size=512)
 
     # Find data files.
@@ -246,8 +413,14 @@ def train_challenge_model(data_folder, model_folder, verbose):
 
 
 def run_challenge_model(model, data, recordings, verbose, thresholds=None, use_cache=False):
+=======
+    # download the pretrained model
+    download_model_from_url('https://raw.githubusercontent.com/maraujo/physionet22/dewen/models/model.pth?token=GHSAT0AAAAAABSVXRZ6SQ5LVHOK7SDHWIFCYSXUQSQ',model_folder,'model.pth')
+    return 0
+>>>>>>> ad44dd9 (change to download from url)
 
     classes = ['Present', 'Unknown', 'Absent']
+<<<<<<< HEAD
     classifier = model # ['classifier']
     fields = data.split("\n")[0].split(" ")
 
@@ -296,6 +469,49 @@ def run_challenge_model(model, data, recordings, verbose, thresholds=None, use_c
         labels = [0, 0, 1]
 
     probabilities = labels
+=======
+    all_probabilities = []
+    all_labels = []
+    # do prediction for each recording
+    if verbose:
+        patient_id = get_patient_id(data)
+        print(f'processing patient_id:{patient_id}')
+    with torch.no_grad():
+        for recording in recordings:
+            # normalized recording
+            recording_mean = recording.mean()
+            recording_std = recording.std()
+            recording = (recording - recording_mean) / recording_std
+            current_recording_length = recording.shape[0]
+            # divide the whole recording into non-overlap segments, and predict each segment
+            if current_recording_length > SEQ_LENGTH:
+                start_point = 0
+                steps = []
+                while (start_point + SEQ_LENGTH) < current_recording_length:
+                    steps += [start_point]
+                    start_point += SEQ_LENGTH # non-overlap
+                steps += [current_recording_length - SEQ_LENGTH]
+                # print(f'steps:{steps}')
+                recording_segments = []
+                for step in steps:
+                    recording_segments.append(recording[step:step+SEQ_LENGTH])
+                recording_segment = torch.from_numpy(np.stack(recording_segments)[:,np.newaxis,:]).float()
+            else: # padding with zeros
+                recording_segment = np.zeros(SEQ_LENGTH)
+                recording_segment[0:current_recording_length] = recording
+                recording_segment = torch.from_numpy(recording_segment[None,None]).float()
+            # make prediction
+            out = model(recording_segment)
+            out_softmax = F.softmax(out, dim=1)
+            out_argmax = torch.argmax(out_softmax, dim=1)
+            # if there are multiple segments in this recording, the final prediction is the one with the largest "Present" probability
+            out_softmax_0 = out_softmax[:,0]
+            max_0_index = torch.argmax(out_softmax_0, dim=0)
+            all_probabilities.append(out_softmax[max_0_index].detach().cpu().numpy())
+            all_labels.append(out_argmax[max_0_index].detach().cpu().numpy())
+    labels = to_one_hot_bool(min(all_labels), len(classes))
+    probabilities = all_probabilities[all_labels.index(min(all_labels))]
+>>>>>>> ad44dd9 (change to download from url)
     return classes, labels, probabilities
 
 
@@ -374,3 +590,86 @@ def get_features(data, recordings):
     features = np.hstack(([age], sex_features, [height], [weight], [is_pregnant], recording_features))
 
     return np.asarray(features, dtype=np.float32)
+<<<<<<< HEAD
+=======
+
+def to_one_hot_bool(data, num_classes):
+    """
+    convert a numpy array to one hot bool
+    data: [N] the array that needs to be converted
+    num_classes: how many classes in this array
+    """
+    result = np.eye(num_classes)[data].astype(np.bool_)
+    return result
+
+def download_url_to_file(url, dst, hash_prefix=None, progress=True):
+    r"""Download object at the given URL to a local path.
+
+    Args:
+        url (string): URL of the object to download
+        dst (string): Full path where object will be saved, e.g. ``/tmp/temporary_file``
+        hash_prefix (string, optional): If not None, the SHA256 downloaded file should start with ``hash_prefix``.
+            Default: None
+        progress (bool, optional): whether or not to display a progress bar to stderr
+            Default: True
+
+    Example:
+        >>> torch.hub.download_url_to_file('https://s3.amazonaws.com/pytorch/models/resnet18-5c106cde.pth', '/tmp/temporary_file')
+
+    """
+    file_size = None
+    req = Request(url, headers={"User-Agent": "torch.hub"})
+    u = urlopen(req)
+    meta = u.info()
+    if hasattr(meta, 'getheaders'):
+        content_length = meta.getheaders("Content-Length")
+    else:
+        content_length = meta.get_all("Content-Length")
+    if content_length is not None and len(content_length) > 0:
+        file_size = int(content_length[0])
+
+    # We deliberately save it in a temp file and move it after
+    # download is complete. This prevents a local working checkpoint
+    # being overridden by a broken download.
+    dst = os.path.expanduser(dst)
+    dst_dir = os.path.dirname(dst)
+    f = tempfile.NamedTemporaryFile(delete=False, dir=dst_dir)
+
+    try:
+        if hash_prefix is not None:
+            sha256 = hashlib.sha256()
+        with tqdm(total=file_size, disable=not progress,
+                  unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+            while True:
+                buffer = u.read(8192)
+                if len(buffer) == 0:
+                    break
+                f.write(buffer)
+                if hash_prefix is not None:
+                    sha256.update(buffer)
+                pbar.update(len(buffer))
+
+        f.close()
+        if hash_prefix is not None:
+            digest = sha256.hexdigest()
+            if digest[:len(hash_prefix)] != hash_prefix:
+                raise RuntimeError('invalid hash value (expected "{}", got "{}")'
+                                   .format(hash_prefix, digest))
+        shutil.move(f.name, dst)
+    finally:
+        f.close()
+        if os.path.exists(f.name):
+            os.remove(f.name)
+
+def download_model_from_url(url, model_dir, file_name):
+    os.makedirs(model_dir, exist_ok=True)
+    parts = urlparse(url)
+    filename = os.path.basename(parts.path)
+    if file_name is not None:
+        filename = file_name
+    cached_file = os.path.join(model_dir, filename)
+    if not os.path.exists(cached_file):
+        sys.stderr.write('Downloading: "{}" to {}\n'.format(url, cached_file))
+        hash_prefix = None
+        download_url_to_file(url, cached_file, hash_prefix, progress=True)
+>>>>>>> ad44dd9 (change to download from url)

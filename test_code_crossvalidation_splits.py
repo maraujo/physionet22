76bd28863e3e056eff329af8d629a759baa5bb1a
matheus <0matheus.araujo0@gmail.com>
2022-04-21 05:12:15 +0000

    set serious test code crossvalidation

--- test_code_crossvalidation_splits.py
+++ test_code_crossvalidation_splits.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-commits = ["14cbe8e6b381147622e8f3137db48833193fd700", "05ce59fdd67e2086bcfc47f0c9ee50dbfc6a39fb", "a9827a4dbed3793a5cd32112708b7786d951b159"]
+commits = ["a9827a4dbed3793a5cd32112708b7786d951b159","05ce59fdd67e2086bcfc47f0c9ee50dbfc6a39fb", "14cbe8e6b381147622e8f3137db48833193fd700"]
 #Run image against crossvalidation
 N_FOLDERS = 10
 results = []
@@ -12,11 +12,11 @@ for commit in commits:
     os.system("docker build -t image .")
     for fold in range(N_FOLDERS):
         print("Commit: {} Fold: {}".format(commit, fold))
-        folder = "/physionet_data/challenge/files/cross-validation-data-mini/{}/".format(fold)
+        folder = "/physionet_data/challenge/files/cross-validation-data/{}/".format(fold)
         os.system("docker run -v {}model:/physionet/model -v {}/test:/physionet/test_data -v {}/test_outputs:/physionet/test_outputs/ -v {}/train/:/physionet/training_data image bash ./test_in_docker.bash".format(folder,folder,folder,folder))
         script_path = folder + "model/result.csv"
         result = pd.read_csv(script_path)
         result["name"] = commit
         result["fold"] = fold
         results.append(result.iloc[0])
-pd.DataFrame(results).to_csv("../final_result.csv")
+        pd.DataFrame(results).to_csv("../final_result.csv")

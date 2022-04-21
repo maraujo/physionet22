e92493a0e1f09bd53c2b41159df4a82969f9b16e
matheus <0matheus.araujo0@gmail.com>
2022-04-21 04:13:28 +0000

    tentative final version to test code cross validation

--- test_code_crossvalidation_splits.py
+++ test_code_crossvalidation_splits.py
@@ -5,19 +5,17 @@ commits = ["a9827a4dbed3793a5cd32112708b7786d951b159", "14cbe8e6b381147622e8f313
 N_FOLDERS = 10
 results = []
 for commit in commits:
-    # os.system("git checkout {}".format(commit))
-    # os.system("cp ../evaluate_model.py ./")
-    # os.system("cp ../test_in_docker.bash ./")
-    # os.system("docker build -t image .")
-    #copy new files
-    #build docker
+    os.system("git checkout {}".format(commit))
+    os.system("cp ../evaluate_model.py ./")
+    os.system("cp ../test_in_docker.bash ./")
+    os.system("docker build -t image .")
     for fold in range(N_FOLDERS):
-        folder = "/physionet_data/challenge/files/cross-validation-data/{}/".format(fold)
+        print("Commit: {} Fold: {}".format(commit, fold))
+        folder = "/physionet_data/challenge/files/cross-validation-data-mini/{}/".format(fold)
         os.system("docker run -v {}model:/physionet/model -v {}/test:/physionet/test_data -v {}/test_outputs:/physionet/test_outputs/ -v {}/train/:/physionet/training_data image bash ./test_in_docker.bash".format(folder,folder,folder,folder))
         script_path = folder + "model/result.csv"
         result = pd.read_csv(script_path)
         result["name"] = commit
         result["fold"] = fold
         results.append(result.iloc[0])
-        import ipdb;ipdb.set_trace()
-        pass
+pd.DataFrame(results).to_csv("../final_result.csv")

b0d184cc5eb7fcb33618bb6c91c00c2b9323de79
matheus <0matheus.araujo0@gmail.com>
2022-05-12 04:16:50 +0000

    test code

--- test_code_crossvalidation_splits.py
+++ test_code_crossvalidation_splits.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-commits = ["a9827a4dbed3793a5cd32112708b7786d951b159","05ce59fdd67e2086bcfc47f0c9ee50dbfc6a39fb", "14cbe8e6b381147622e8f3137db48833193fd700"]
+commits = ["b8501999a67f2a486da7d644d4c0bacb677b61be"]
 #Run image against crossvalidation
 N_FOLDERS = 10
 results = []
@@ -12,7 +12,7 @@ for commit in commits:
     os.system("docker build -t image .")
     for fold in range(N_FOLDERS):
         print("Commit: {} Fold: {}".format(commit, fold))
-        folder = "/physionet_data/challenge/files/cross-validation-data/{}/".format(fold)
+        folder = "/physionet_data/challenge/files/cross-validation-data-1-0-3/{}/".format(fold)
         os.system("docker run -v {}model:/physionet/model -v {}/test:/physionet/test_data -v {}/test_outputs:/physionet/test_outputs/ -v {}/train/:/physionet/training_data image bash ./test_in_docker.bash".format(folder,folder,folder,folder))
         script_path = folder + "model/result.csv"
         result = pd.read_csv(script_path)
